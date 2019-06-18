7fef801ebdfa9e89fc7042213dd19d457d929261
Rafael Marques Criado <rafael.marquescriado@gmail.com>
2019-06-18 11:19:22 -0300

    correcao botao saiba mais

--- hotsite/resources/views/templates/conheca.blade.php
+++ hotsite/resources/views/templates/conheca.blade.php
@@ -3,7 +3,7 @@
         <div class="row">
             <div class="col-xs-12 col-md-12 col-lg-12">
                 <h2 class="title">Conheça mais a <span>via</span></h2>
-                <a href="{{ route('sobre') }}">Saiba mais</a>
+                <a href="http://www.via.com.vc/" target="blank"Saiba mais</a>
             </div>
         </div>
     </div>
