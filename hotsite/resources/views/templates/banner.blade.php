c2ae2979623eac684d59e74a3231f8ef8724e954
Rafael Marques Criado <rafael.marquescriado@gmail.com>
2019-06-18 14:23:17 -0300

    download

--- hotsite/resources/views/templates/banner.blade.php
+++ hotsite/resources/views/templates/banner.blade.php
@@ -44,9 +44,9 @@
                         <div>
                             <input type="email" name="email" placeholder="E-mail" required>
                             <div hidden="hidden" class="fields-hidden">
-                                <input type="text" name="nomee" placeholder="Nome completo" required>
-                                <input type="text" name="nomee-empresa" placeholder="Nome da empresa" required>
-                                <input type="tel" name="celulear" placeholder="Celular" class="sp_celphones" required>
+                                <input type="text" name="name" placeholder="Nome completo" required>
+                                <input type="text" name="empresa" placeholder="Nome da empresa" required>
+                                <input type="tel" name="celular" placeholder="Celular" class="sp_celphones" required>
                                 <input type="submit" class="submit" placeholder="Baixar agora">
                             </div>
                             <a href="javascript:void(0)" class="show-fields">Baixar agora</a>
