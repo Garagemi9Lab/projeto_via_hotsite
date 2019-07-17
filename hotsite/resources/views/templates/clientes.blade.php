b1951564292380cd85757b45afe2ef02edf3b250
Rafael Criado <rafael.marquescriado@gmail.com>
2019-07-17 09:21:18 -0300

    Logos dos clientes

--- hotsite/resources/views/templates/clientes.blade.php
+++ hotsite/resources/views/templates/clientes.blade.php
@@ -6,24 +6,22 @@
 
                 <div class="customers-slider">
                     
+                    <div><img class="customers-logo" src="{{ asset('via/assets/images/clientes/1.png') }}"></div> 
                     <div><img class="customers-logo" src="{{ asset('via/assets/images/clientes/2.png') }}"></div> 
                     <div><img class="customers-logo" src="{{ asset('via/assets/images/clientes/3.png') }}"></div> 
-                    <div><img class="customers-logo" src="{{ asset('via/assets/images/clientes/5.jpg') }}"></div> 
+                    <div><img class="customers-logo" src="{{ asset('via/assets/images/clientes/4.png') }}"></div> 
+                    <div><img class="customers-logo" src="{{ asset('via/assets/images/clientes/5.png') }}"></div> 
                     <div><img class="customers-logo" src="{{ asset('via/assets/images/clientes/6.png') }}"></div> 
                     <div><img class="customers-logo" src="{{ asset('via/assets/images/clientes/7.png') }}"></div> 
                     <div><img class="customers-logo" src="{{ asset('via/assets/images/clientes/8.png') }}"></div> 
-                    <div><img class="customers-logo" src="{{ asset('via/assets/images/clientes/9.jpg') }}"></div> 
+                    <div><img class="customers-logo" src="{{ asset('via/assets/images/clientes/9.png') }}"></div> 
                     <div><img class="customers-logo" src="{{ asset('via/assets/images/clientes/10.png') }}"></div> 
                     <div><img class="customers-logo" src="{{ asset('via/assets/images/clientes/11.png') }}"></div> 
+                    <div><img class="customers-logo" src="{{ asset('via/assets/images/clientes/12.png') }}"></div> 
                     <div><img class="customers-logo" src="{{ asset('via/assets/images/clientes/13.png') }}"></div> 
-                    <div><img class="customers-logo" src="{{ asset('via/assets/images/clientes/14.jpg') }}"></div> 
-                    <div><img class="customers-logo" src="{{ asset('via/assets/images/clientes/16.png') }}"></div> 
-                    <div><img class="customers-logo" src="{{ asset('via/assets/images/clientes/17.png') }}"></div> 
-                    <div><img class="customers-logo" src="{{ asset('via/assets/images/clientes/18.png') }}"></div>
-                    <div><img class="customers-logo" src="{{ asset('via/assets/images/clientes/19.png') }}"></div> 
+                    <div><img class="customers-logo" src="{{ asset('via/assets/images/clientes/14.png') }}"></div> 
                     
                 </div>
-
             </div>
         </div>
     </div>
